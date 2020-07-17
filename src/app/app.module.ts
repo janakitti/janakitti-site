11a7fbcfa6ea75519133d32e90bf1d8aa9c6c6bc
Janakitti <janakittiratana@gmail.com>
2020-07-16 21:37:49 -0400

    Add home and body components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
+import { LandingComponent } from './home/landing/landing.component';
+import { BodyComponent } from './home/body/body.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    BodyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
