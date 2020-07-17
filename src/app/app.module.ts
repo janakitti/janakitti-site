e5e18faaef4b1711f7e03a5bf4b84cabae8ad597
Janakitti <janakittiratana@gmail.com>
2020-07-16 23:38:53 -0400

    Add about-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './home/landing/landing.component';
 import { BodyComponent } from './home/body/body.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './home/body/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     BodyComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
