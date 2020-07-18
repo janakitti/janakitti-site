4bda88ee6e80063742a7abb2b7ecb5cfd7e9471c
Janakitti <janakittiratana@gmail.com>
2020-07-17 23:33:59 -0400

    Add website progress bar

--- src/app/home/body/progress-bars/progress-bars.component.ts
+++ src/app/home/body/progress-bars/progress-bars.component.ts
@@ -9,7 +9,8 @@ import {ProgressItems} from '../../../shared/interfaces';
 export class ProgressBarsComponent implements OnInit {
   progressItems: ProgressItems[] = [
     { name: 'High School', percentage: 100, color1: '#ff6e63', color2: '#fc7e74' },
-    { name: 'B.Eng SE', percentage: 25, color1: '#a13cc9', color2: '#ae54d1' }
+    { name: 'B.Eng SE', percentage: 25, color1: '#a13cc9', color2: '#ae54d1' },
+    { name: 'This website', percentage: 8, color1: '#76dec2', color2: '#8fe3cc' }
   ];
   constructor() { }
 
