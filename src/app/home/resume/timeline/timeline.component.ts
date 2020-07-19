06dcfa42689dee447ee2503a3b4a922cd63b60d2
Janakitti <janakittiratana@gmail.com>
2020-07-18 21:30:29 -0400

    Begin timeline on right

--- src/app/home/resume/timeline/timeline.component.ts
+++ src/app/home/resume/timeline/timeline.component.ts
@@ -10,16 +10,16 @@ declare var $: any;
 })
 export class TimelineComponent implements OnInit {
   achievements: Achievement[] = [
-    { title: 'DECA Provincial Champion', date: 'Jan 2020'},
-    { title: 'DECA Provincial Champion', date: 'Aug 2019'},
-    { title: ' University of Waterloo President\'s Scholarship', date: 'Jan 2020'},
+    { title: 'Ted Rogers Scholar', date: 'Aug 2019'},
     { title: 'Schulich Leader\'s Scholarship Winner', date: 'Jan 2020'},
-    { title: 'Ted Rogers Scholar', date: 'Aug 2019'}
+    { title: ' University of Waterloo President\'s Scholarship', date: 'Jan 2020'},
+    { title: 'DECA Provincial Champion', date: 'Aug 2019'},
+    { title: 'DECA Provincial Champion', date: 's'},
   ];
   constructor() { }
 
   ngOnInit(): void {
-
+    $('#timeline-container-scroll').scrollLeft(1000);
   }
 
 }
