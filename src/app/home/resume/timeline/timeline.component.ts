b511498acb85aadb851bc8cea3594acb1a78bf8e
Janakitti <janakittiratana@gmail.com>
2020-07-18 23:22:21 -0400

    Fix dates on timeline

--- src/app/home/resume/timeline/timeline.component.ts
+++ src/app/home/resume/timeline/timeline.component.ts
@@ -10,11 +10,11 @@ declare var $: any;
 })
 export class TimelineComponent implements OnInit {
   achievements: Achievement[] = [
-    { title: 'Ted Rogers Scholar', date: 'Aug 2019'},
-    { title: 'Schulich Leader\'s Scholarship Winner', date: 'Jan 2020'},
-    { title: ' University of Waterloo President\'s Scholarship', date: 'Jan 2020'},
-    { title: 'DECA Provincial Champion', date: 'Aug 2019'},
-    { title: 'DECA Provincial Champion', date: 's'},
+    { title: 'Ted Rogers Scholar', date: '2019'},
+    { title: 'Schulich Leader\'s Scholarship Winner', date: '2019'},
+    { title: ' University of Waterloo President\'s Scholarship', date: '2019'},
+    { title: 'DECA Provincial Champion', date: 'Apr 2019'},
+    { title: 'DECA Provincial Champion', date: 'Apr 2018'},
   ];
   constructor() { }
 
