8f865b1f65387698cf12dc352f6482228b0ad692
Janakitti <janakittiratana@gmail.com>
2020-07-18 20:43:28 -0400

    Reformat and add content

--- src/app/home/resume/resume.component.ts
+++ src/app/home/resume/resume.component.ts
@@ -18,6 +18,14 @@ export class ResumeComponent implements OnInit {
           'Front-end work']
       },
       {
+        title: 'FedoraField',
+        date: 'Oct 2019 - present',
+        tech: ['C#', 'Unity', 'Adobe Illustrator'],
+        description: ['Developed a game in which players interact with enemy projectiles using gravitational and magnetic fields',
+          'Modelled object interactions with various fields using the Unity physics engine',
+          'Created a level loading system and an adaptive soundtrack using Unity coroutines']
+      },
+      {
         title: 'Tangible',
         date: 'Oct 2019 - Dec 2019',
         tech: ['Arduino', 'C++', 'Unity'],
@@ -29,12 +37,38 @@ export class ResumeComponent implements OnInit {
     ],
     experiences: [
       {
-        organization: 'Example',
-        position: 'Coop',
+        organization: 'Providius Corp',
+        position: 'Software Engineering Co-op',
         date: 'May 2020 - present',
-        location: 'yes',
-        description: ['Sample text']
-      }
+        location: 'Hamilton, ON',
+        description: ['Developed Angular/Python web app for simulating real world data flow on a network']
+      },
+      {
+        organization: 'Hack the North',
+        position: 'Hacker',
+        date: '2018 & 2019',
+        location: 'Waterloo, ON',
+        description: ['Participated as a hackathon hacker at Canada\'s biggest hackathon',
+          '2018 | MAGNET: a projectile-catching robot',
+          '2019 | CamHunt: a mobile app protoype for playing man-hunt using your mobile camera']
+      },
+      {
+        organization: 'Team 4308: Absolute Robotics - FIRST Robotics',
+        position: 'Programmer',
+        date: 'Sep 2017 - Apr 2019',
+        location: 'Mississauga, ON',
+        description: ['Developed the computer vision pipeline to identify target objects on the playing field using Python',
+                      'Qualified as 2018 FIRST Robotics World Championship Divisional Semi-Finalists',
+                      'Rebranded the Absolute Robotics organization, increasing student engagement within the school']
+      },
+      {
+        organization: 'DECA Business Competition',
+        position: 'Entrepreneurial Competitor',
+        date: 'Sep 2017 - Apr 2019',
+        location: 'Mississauga, ON',
+        description: ['Prepared a business plan and pitch for a startup company idea',
+          'Competed at the international competition in Atlanta, Georgia']
+      },
     ]
   };
 
