1708a77701b10ba20ad246e7082e35ff015c829d
Janakitti <janakittiratana@gmail.com>
2020-07-20 21:32:50 -0400

    Add featured-card component, fix title scaling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ResumeComponent } from './home/resume/resume.component';
 import { TimelineComponent } from './home/resume/timeline/timeline.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FeaturedCardComponent } from './home/body/progress-bars/featured-card/featured-card.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { HttpClientModule } from '@angular/common/http';
     FeaturedComponent,
     FooterComponent,
     ResumeComponent,
-    TimelineComponent
+    TimelineComponent,
+    FeaturedCardComponent
   ],
   imports: [
     BrowserModule,
