ec2b30193dd646aa8e072874f866c20b79959331
Janakitti <janakittiratana@gmail.com>
2020-07-21 22:24:58 -0400

    Create default blank values for resume on load

--- src/app/home/resume/resume.component.ts
+++ src/app/home/resume/resume.component.ts
@@ -9,7 +9,7 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent implements OnInit {
-  resume: Resume;
+  resume: Resume = {projects: [], experiences: []};
 
   constructor(public formatService: FormattingService, private http: HttpClient) {
     this.http.get<Resume>('../../../assets/resume-data.json').subscribe(data => {
