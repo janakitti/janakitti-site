335470d3ed32952a011abb3d789c0fa4132ec88f
Janakitti <janakittiratana@gmail.com>
2020-09-01 15:39:09 -0400

    Replace Coop progress with ML progress

--- src/app/home/body/progress-bars/progress-bars.component.ts
+++ src/app/home/body/progress-bars/progress-bars.component.ts
@@ -13,12 +13,12 @@ export class ProgressBarsComponent implements OnInit {
     const seStart = new Date('September 7, 2019, 00:00:00').getTime();
     const seEnd = new Date('April 31, 2024, 00:00:00').getTime();
     const sePercentage = Math.round((today - seStart) / (seEnd - seStart) * 100);
-    const coopStart = new Date('May 4, 2020, 00:00:00').getTime();
-    const coopEnd = new Date('August 28, 2020, 00:00:00').getTime();
+    const coopStart = new Date('September 1, 2020, 00:00:00').getTime();
+    const coopEnd = new Date('December 1, 2020, 00:00:00').getTime();
     const coopPercentage = Math.round((today - coopStart) / (coopEnd - coopStart) * 100);
     this.progressItems = [
-      { name: 'Coop @ Providius Corp', percentage: coopPercentage, color1: '#69dbce', color2: '#7be0d5' },
-      { name: 'Bachelor of Software Engineering', percentage: sePercentage, color1: '#a13cc9', color2: '#ae54d1' }
+      { name: 'Bachelor of Software Engineering', percentage: sePercentage, color1: '#a13cc9', color2: '#ae54d1' },
+      { name: 'Machine Learning by Stanford University', percentage: coopPercentage, color1: '#c4294b', color2: '#d14765' }
     ];
   }
 
