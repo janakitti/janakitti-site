39f77c5cfcea9ff457e8bfcc31f933d8402660d8
Janakitti <janakittiratana@gmail.com>
2020-09-21 23:04:01 -0400

    Implement resume/carousel toggle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public isCarouselMode = false;
   constructor() {}
 
   ngOnInit(): void {
   }
+
+ toggleView(el: HTMLElement) {
+    this.isCarouselMode = !this.isCarouselMode;
+    el.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+  }
+
 
 }
