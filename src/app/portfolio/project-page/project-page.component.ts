37029f7a284051c47fd7fd9c6cf87c5db31f4926
Janakitti <janakittiratana@gmail.com>
2021-01-03 14:57:57 -0500

    Add content to project pages

--- src/app/portfolio/project-page/project-page.component.ts
+++ src/app/portfolio/project-page/project-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {DataService} from '../../core/data.service';
 import {ProjectPage} from '../../shared/interfaces';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-project-page',
@@ -14,7 +15,7 @@ export class ProjectPageComponent implements OnInit {
     body: []
   };
 
-  constructor(public dataService: DataService, private route: ActivatedRoute) { }
+  constructor(public dataService: DataService, private route: ActivatedRoute, private _sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.getData();
@@ -24,5 +25,9 @@ export class ProjectPageComponent implements OnInit {
     const project = this.route.snapshot.paramMap.get('project');
     this.dataService.getProject()
       .subscribe(data => this.pageData = data[project]);
+  }
+
+  sanitizeUrl(url: string): any {
+    return this._sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
