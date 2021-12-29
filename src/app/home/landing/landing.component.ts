a1f5900f5c7f4ac7c5f2f0a0cac5486370afff54
janakitti <janakittiratana@gmail.com>
2021-12-28 23:26:40 -0500

    Change size of shard based on screen width, prettify code

--- src/app/home/landing/landing.component.ts
+++ src/app/home/landing/landing.component.ts
@@ -19,42 +19,54 @@ export class LandingComponent implements AfterViewInit {
 
   private createScene() {
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
 
-    this.renderer = new THREE.WebGLRenderer( { alpha: true } );
-    this.renderer.setClearColor( 0x000000, 0 );
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer = new THREE.WebGLRenderer({ alpha: true });
+    this.renderer.setClearColor(0x000000, 0);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.physicallyCorrectLights = true;
-    this.renderer.outputEncoding =  THREE.sRGBEncoding;
-    this.renderer.gammaOutput =  true;
-    this.renderer.gammaFactor =  2.2;
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.gammaOutput = true;
+    this.renderer.gammaFactor = 2.2;
 
-    document.getElementById("landing-img").appendChild(this.renderer.domElement);
+    document
+      .getElementById('landing-img')
+      .appendChild(this.renderer.domElement);
 
     this.loader = new GLTFLoader();
-    this.loader.load( '../../../assets/3d/shard0.glb', ( gltf: GLTF ) => {
-      this.model = gltf.scene;
-      this.model.traverse(child => {
-        if (child.isMesh === true){
+    this.loader.load(
+      '../../../assets/3d/shard0.glb',
+      (gltf: GLTF) => {
+        this.model = gltf.scene;
+        this.model.traverse((child) => {
+          if (child.isMesh === true) {
             child.material.transparent = true;
             child.material.opacity = 0;
-        }
-      });
-      this.scene.add(this.model);
-    }, undefined, error => {
-      console.error( error );
-    });
+          }
+        });
+        this.scene.add(this.model);
+      },
+      undefined,
+      (error) => {
+        console.error(error);
+      }
+    );
 
-    const ambientLight = new THREE.AmbientLight( 0xffffff );
-    this.scene.add( ambientLight );
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    this.scene.add(ambientLight);
 
-    var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-    hemiLight.position.set( 0, 10, 5 );
-    this.scene.add( hemiLight );
+    var hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+    hemiLight.position.set(0, 10, 5);
+    this.scene.add(hemiLight);
 
-    var dirLight = new THREE.DirectionalLight( 0xffffff );
-    dirLight.position.set( 0, 0, 5 );
-    this.scene.add( dirLight );
+    var dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(0, 0, 5);
+    this.scene.add(dirLight);
 
     this.camera.position.z = 6;
     this.camera.rotation.z = 0.65;
@@ -65,30 +77,35 @@ export class LandingComponent implements AfterViewInit {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
     // Update the camera
-    this.camera.aspect = window.innerWidth/window.innerHeight;
+    this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
+
+    if (window.innerWidth < 768) {
+      this.camera.position.z = 12;
+    } else {
+      this.camera.position.z = 6;
+    }
   }
 
   private startRenderingLoop() {
     let component: LandingComponent = this;
     function animate() {
-      requestAnimationFrame( animate );
+      requestAnimationFrame(animate);
       if (component.isResized) {
         component.resizeCanvasToDisplaySize();
       }
 
-      if (component.model)
-      {
+      if (component.model) {
         component.model.rotation.y += 0.002;
       }
-      component.renderer.render( component.scene, component.camera );
-    };
+      component.renderer.render(component.scene, component.camera);
+    }
     animate();
   }
 
   ngAfterViewInit(): void {
-    window.addEventListener('resize', () => this.isResized = true);
+    window.addEventListener('resize', () => (this.isResized = true));
     this.createScene();
-    this.startRenderingLoop(); 
+    this.startRenderingLoop();
   }
 }
