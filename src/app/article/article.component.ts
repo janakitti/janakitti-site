0f3b19ea0f18cda1fa3090fb18a4a36a39d0e795
janakitti <janakittiratana@gmail.com>
2024-06-09 11:31:52 -0400

    Create Article component

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.css',
+})
+export class ArticleComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private _sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    const data = this.route.snapshot.paramMap.get('data');
+    console.log(data);
+  }
+
+  sanitizeUrl(url: string): any {
+    return this._sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
