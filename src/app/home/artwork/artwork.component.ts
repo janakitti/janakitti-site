dd170967ce48aebf0f98e76f967c1a82ee99c4de
janakitti <janakittiratana@gmail.com>
2024-06-27 15:55:47 -0400

    Link cards to Instagram pages

--- src/app/home/artwork/artwork.component.ts
+++ src/app/home/artwork/artwork.component.ts
@@ -15,21 +15,25 @@ export class ArtworkComponent {
       id: 'googledoodle',
       name: 'Google Doodle',
       imgUrl: '/artwork/googledoodle.png',
+      url: 'https://www.instagram.com/artwork.jr',
     },
     {
       id: 'hidden',
       name: 'Hidden',
       imgUrl: '/artwork/hidden.png',
+      url: 'https://www.instagram.com/artwork.jr',
     },
     {
       id: 'ghostcrew',
       name: 'Ghost Crew',
       imgUrl: '/artwork/ghostcrew.png',
+      url: 'https://www.instagram.com/artwork.jr',
     },
     {
       id: 'queensgambit',
       name: "The Queen's Gambit",
       imgUrl: '/artwork/queens_gambit.png',
+      url: 'https://www.instagram.com/artwork.jr',
     },
   ];
 }
--- src/app/home/photos/photos.component.ts
+++ src/app/home/photos/photos.component.ts
@@ -15,56 +15,67 @@ export class PhotosComponent {
       id: 'cliffhanger',
       name: '',
       imgUrl: '/photos/cliffhanger.jpg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'doi_inthanon_flowers',
       name: '',
       imgUrl: '/photos/doi_inthanon_flowers.jpg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'snorkel',
       name: '',
       imgUrl: '/photos/snorkel.jpeg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'leaves',
       name: '',
       imgUrl: '/photos/leaves.jpg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'mori_point',
       name: '',
       imgUrl: '/photos/mori_point.jpg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'temple_dog',
       name: '',
       imgUrl: '/photos/temple_dog.jpg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'rice_fields',
       name: '',
       imgUrl: '/photos/rice_fields.jpg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'temple',
       name: '',
       imgUrl: '/photos/temple.jpg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'yaowarat',
       name: '',
       imgUrl: '/photos/yaowarat.jpeg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'palm',
       name: '',
       imgUrl: '/photos/palm.jpg',
+      url: 'https://www.instagram.com/janakitti',
     },
     {
       id: 'motorcycle',
       name: '',
       imgUrl: '/photos/motorcycle.jpg',
+      url: 'https://www.instagram.com/janakitti',
     },
   ];
 }
